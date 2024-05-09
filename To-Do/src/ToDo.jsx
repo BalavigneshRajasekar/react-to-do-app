9e32d2b75ddcc97e7728def31a2fbc72cb2dafa4
BalavigneshRajasekar <you@example.com>
2024-05-09 21:35:22 +0530

    Added edit and delete actions

--- To-Do/src/ToDo.jsx
+++ To-Do/src/ToDo.jsx
@@ -10,6 +10,8 @@ function ToDo() {
   });
   let [filter, setFilter] = useState("All");
   let [filters, setFilters] = useState([]);
+  let [showBtn, setShowBtn] = useState(true);
+  let [index, setIndex] = useState();
 
   function handleData(e) {
     setFormData({ ...formData, [e.target.name]: e.target.value });
@@ -30,27 +32,45 @@ function ToDo() {
     return todo.status === filter;
   });
 
-  function handleSelect(e, index, value) {
-    // if (e.target.value == "Completed") {
-    //   e.target.style.backgroundColor = "green";
-    // } else if (e.target.value == "Not Completed") {
-    //   e.target.style.backgroundColor = "red";
-    // }
-
+  function handleSelect(e, index) {
     filteredTodo[index].status = e.target.value;
     setFilters(filteredTodo);
+  }
 
-    console.log(storeData);
-    console.log(value);
-    console.log(index);
-    console.log(storeData);
+  function handleDelete(index) {
+    let deletedData = filteredTodo.filter((_, index1) => {
+      return index1 !== index;
+    });
+    setStoreData(deletedData);
+  }
+
+  function handleEdit(value, index) {
+    setFormData({
+      task: value.task,
+      description: value.description,
+      status: "Not Completed",
+    });
+    setShowBtn(false);
+    setIndex(index);
+  }
+
+  function HandleUpdate() {
+    console.log(formData);
+    storeData[index] = formData;
+
+    setShowBtn(true);
+    setFormData({
+      task: "",
+      description: "",
+      status: "Not Completed",
+    });
   }
   return (
-    <div className="container-fluid ">
+    <div className="container ">
       <div>
         <input
           type="text"
-          className="input-field"
+          className="input-field "
           value={formData.task}
           onChange={handleData}
           placeholder="task name"
@@ -58,26 +78,37 @@ function ToDo() {
         ></input>
         <input
           type="text"
-          className="input-field"
+          className="input-field "
           placeholder="description"
           value={formData.description}
           name="description"
           onChange={handleData}
         ></input>
-        <button
-          className="btn btn-success px-5 ms-5 mt-1 mb-2"
-          onClick={addTaskData}
-        >
-          Add
-        </button>
+        {showBtn ? (
+          <button
+            className="btn btn-success px-5 ms-md-5 mt-4 mt-md-1 mb-2"
+            onClick={addTaskData}
+          >
+            Add
+          </button>
+        ) : (
+          <button
+            className="btn btn-success px-5 ms-md-5 mt-4 mt-md-1 mb-2"
+            onClick={() => HandleUpdate()}
+          >
+            Update
+          </button>
+        )}
       </div>
+
+      {/* Filter bar */}
 
       <div className="d-flex justify-content-between mt-5">
         <h5>My ToDos</h5>
         <h5>
           Filters:
           <select
-            className="select "
+            className="select py-1"
             onChange={(e) => setFilter(e.target.value)}
           >
             <option value="All">All</option>
@@ -90,29 +121,39 @@ function ToDo() {
       {/* TODO CARD */}
 
       <div className="mt-4 d-flex justify-content-around gap-3 flex-wrap">
-        {filteredTodo.map((value, index) => (
+        {filteredTodo.map((values, index) => (
           <div className="card" style={{ width: "20rem" }} key={index}>
             <div className="card-body">
-              <h6 className="card-title">Task : {value.task}</h6>
-              <p className="card-text">Description : {value.description}</p>
+              <h6 className="card-title">Task : {values.task}</h6>
+              <p className="card-text">Description : {values.description}</p>
               <p>
                 Status :
                 <select
                   className={
-                    value.status == "Completed"
-                      ? "bg-success text-light"
-                      : "bg-danger text-light"
+                    values.status == "Completed"
+                      ? "bg-success text-light py-1 ms-1"
+                      : "bg-danger text-light py-1 ms-1"
                   }
-                  onChange={(e) => handleSelect(e, index, value)}
-                  value={value.status}
+                  onChange={(e) => handleSelect(e, index)}
+                  value={values.status}
                 >
                   <option value="Not Completed">Not Completed</option>
                   <option value="Completed">Completed</option>
                 </select>
               </p>
               <div className="d-flex justify-content-end">
-                <button className="btn btn-success">Edit</button>
-                <button className="btn btn-danger ms-2">Delete</button>
+                <button
+                  className="btn btn-success"
+                  onClick={() => handleEdit(values, index)}
+                >
+                  Edit
+                </button>
+                <button
+                  className="btn btn-danger ms-2"
+                  onClick={() => handleDelete(index)}
+                >
+                  Delete
+                </button>
               </div>
             </div>
           </div>
